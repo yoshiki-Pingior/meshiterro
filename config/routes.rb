bc60de8095395a26da079511adc03b775dd9f9cb
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-04-09 08:50:47 +0000

    [Finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  
   root to: 'homes#top'
+  root 'post_images#index'
   devise_for :users
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
@@ -7,4 +9,5 @@ Rails.application.routes.draw do
 
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 end
